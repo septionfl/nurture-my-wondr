a4bd3090933d974b8c2a5f5293e96ed01e89840a
septionfl <naufalseptio59@gmail.com>
2026-05-29 22:29:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-// 1. Tambahkan impor Tailwind ini
-import tailwindcss from "@tailwindcss/vite"; 
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    tailwindcss(), 
+    tailwindcss(), // Pastikan posisi Tailwind tetap di atas sini
     tsconfigPaths(),
-    tanstackStart(),
+    tanstackStart(), // Tanpa tambahan preset manual, biarkan sistem yang mendeteksi Vercel
     react(),
   ],
 });
