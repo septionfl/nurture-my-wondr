1c7b72b1570d677f508c68a9c4df57071b062c6b
septionfl <naufalseptio59@gmail.com>
2026-05-29 20:32:18 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart({
-      server: {
-        preset: "vercel", 
-      },
+    tanstackStart(),
+    // Masukkan Nitro dan paksa menjadi format Vercel
+    nitro({
+      preset: "vercel"
     }),
     react(),
   ],
